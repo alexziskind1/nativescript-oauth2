594748a2094def436043d23241f393b232a1f5b3
Alexander Ziskind <alex@nuvious.com>
2018-11-12 11:23:28 -0500

    Added demo-vue project

---
+++ demo-vue/README.md
@@ -0,0 +1,19 @@
+# NativeScript-Vue Application
+
+> A native application built with NativeScript-Vue
+
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Build for production
+tns build <platform> --bundle
+
+# Build, watch for changes and debug the application
+tns debug <platform> --bundle
+
+# Build, watch for changes and run the application
+tns run <platform> --bundle
+```
---
+++ demo-vue/app/components/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <Page>
+        <ActionBar title="OAuth Demo: NativeScript-Vue"/>
+        <StackLayout class="p-20">
+            <Button text="Login" @tap="onLoginTap()" class="btn btn-primary btn-active"/>
+            <Button text="Logout" @tap="onLogoutTap()" class="btn btn-primary btn-active"/>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+    var auth_service_1 = require("../auth-service");
+
+
+  export default {
+    data() {
+      return {}
+    },
+    methods: {
+        onLoginTap() {
+            auth_service_1.tnsOauthLogin("facebook");
+        },
+        onLogoutTap() {
+            auth_service_1.tnsOauthLogout();
+        }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>

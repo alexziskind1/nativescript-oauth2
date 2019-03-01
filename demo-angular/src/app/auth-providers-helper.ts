9db1f01941fdd8379688182f4263c83a57b010f9
Sebastian Gug <sebastian.gug@ishango.co.uk>
2019-03-01 23:46:53 +0000

    fixed this. syntax in angular example that resulted in 'undefined'

--- demo-angular/src/app/auth-providers-helper.ts
+++ demo-angular/src/app/auth-providers-helper.ts
@@ -2,7 +2,7 @@ import {
     TnsOAuthClient,
     configureTnsOAuth,
     ITnsOAuthTokenResult
-} from "nativescript-oauth2";
+} from 'nativescript-oauth2';
 import {
     TnsOaProvider,
     TnsOaProviderOptionsFacebook,
@@ -11,26 +11,26 @@ import {
     TnsOaProviderGoogle,
     TnsOaProviderOptionsMicrosoft,
     TnsOaProviderMicrosoft
-} from "nativescript-oauth2/providers";
+} from 'nativescript-oauth2/providers';
 
 export function configureOAuthProviders() {
-    const microsoftProvider = this.configureOAuthProviderMicrosoft();
-    const googleProvider = this.configureOAuthProviderGoogle();
-    const facebookProvider = this.configureOAuthProviderFacebook();
+    const microsoftProvider = configureOAuthProviderMicrosoft();
+    const googleProvider = configureOAuthProviderGoogle();
+    const facebookProvider = configureOAuthProviderFacebook();
 
     configureTnsOAuth([microsoftProvider, googleProvider, facebookProvider]);
 }
 
 export function configureOAuthProviderGoogle(): TnsOaProvider {
     const googleProviderOptions: TnsOaProviderOptionsGoogle = {
-        openIdSupport: "oid-full",
+        openIdSupport: 'oid-full',
         clientId:
-            "932931520457-buv2dnhgo7jjjjv5fckqltn367psbrlb.apps.googleusercontent.com",
+            '932931520457-buv2dnhgo7jjjjv5fckqltn367psbrlb.apps.googleusercontent.com',
         redirectUri:
-            "com.googleusercontent.apps.932931520457-buv2dnhgo7jjjjv5fckqltn367psbrlb:/auth",
+            'com.googleusercontent.apps.932931520457-buv2dnhgo7jjjjv5fckqltn367psbrlb:/auth',
         urlScheme:
-            "com.googleusercontent.apps.932931520457-buv2dnhgo7jjjjv5fckqltn367psbrlb",
-        scopes: ["email"]
+            'com.googleusercontent.apps.932931520457-buv2dnhgo7jjjjv5fckqltn367psbrlb',
+        scopes: ['email']
     };
     const googleProvider = new TnsOaProviderGoogle(googleProviderOptions);
     return googleProvider;
@@ -38,11 +38,11 @@ export function configureOAuthProviderGoogle(): TnsOaProvider {
 
 export function configureOAuthProviderFacebook(): TnsOaProvider {
     const facebookProviderOptions: TnsOaProviderOptionsFacebook = {
-        openIdSupport: "oid-none",
-        clientId: "691208554415645",
-        clientSecret: "d8725ac416fa1bb1917ccffd1670e3c6",
-        redirectUri: "https://www.facebook.com/connect/login_success.html",
-        scopes: ["email"]
+        openIdSupport: 'oid-none',
+        clientId: '691208554415645',
+        clientSecret: 'd8725ac416fa1bb1917ccffd1670e3c6',
+        redirectUri: 'https://www.facebook.com/connect/login_success.html',
+        scopes: ['email']
     };
     const facebookProvider = new TnsOaProviderFacebook(facebookProviderOptions);
     return facebookProvider;
@@ -50,12 +50,12 @@ export function configureOAuthProviderFacebook(): TnsOaProvider {
 
 export function configureOAuthProviderMicrosoft(): TnsOaProvider {
     const microsoftProviderOptions: TnsOaProviderOptionsMicrosoft = {
-        openIdSupport: "oid-full",
-        clientId: "f376fa87-64a9-49a1-8b56-e0d48fc0810b",
+        openIdSupport: 'oid-full',
+        clientId: 'f376fa87-64a9-49a1-8b56-e0d48fc0810b',
         // redirectUri: "urn:ietf:wg:oauth:2.0:oob",
-        redirectUri: "msalf376fa87-64a9-49a1-8b56-e0d48fc0810b://auth",
-        urlScheme: "msalf376fa87-64a9-49a1-8b56-e0d48fc0810b",
-        scopes: ['openid', "https://outlook.office.com/mail.read"]
+        redirectUri: 'msalf376fa87-64a9-49a1-8b56-e0d48fc0810b://auth',
+        urlScheme: 'msalf376fa87-64a9-49a1-8b56-e0d48fc0810b',
+        scopes: ['openid', 'https://outlook.office.com/mail.read']
     };
     const microsoftProvider = new TnsOaProviderMicrosoft(
         microsoftProviderOptions

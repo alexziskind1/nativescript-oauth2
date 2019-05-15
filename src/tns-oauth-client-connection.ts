17adb928c76325176cafe5336319fac43e2ea0fd
Brian Spencer <bjspencer@xactware.com>
2019-05-15 09:55:30 -0600

    Catch network failures on all HTTP promises

--- src/tns-oauth-client-connection.ts
+++ src/tns-oauth-client-connection.ts
@@ -100,17 +100,25 @@ export class TnsOAuthClientConnection {
         headers: headers,
         content: body
       })
-      .then((response: http.HttpResponse) => {
-        if (response.statusCode !== 200) {
+      .then(
+        (response: http.HttpResponse) => {
+          if (response.statusCode !== 200) {
+            this.completion(
+              null,
+              response,
+              new Error(`Failed logout with status ${response.statusCode}.`)
+            );
+          } else {
+            this.completion(null, response, null);
+          }
+        },
+        error =>
           this.completion(
             null,
-            response,
-            new Error(`Failed logout with status ${response.statusCode}.`)
-          );
-        } else {
-          this.completion(null, response, null);
-        }
-      });
+            error,
+            new Error(`Failed logout due to network error.`)
+          )
+      );
   }
 
   public startTokenRefresh() {
@@ -122,15 +130,19 @@ export class TnsOAuthClientConnection {
     let body = null;
     switch (this.client.provider.options.openIdSupport) {
       case "oid-full":
-        const options1 = <TnsOaOpenIdProviderOptions>this.client.provider.options;
+        const options1 = <TnsOaOpenIdProviderOptions>(
+          this.client.provider.options
+        );
         body = querystring.stringify({
           grant_type: "refresh_token",
           refresh_token: this.client.tokenResult.refreshToken,
           client_id: options1.clientId
         });
         break;
-        case "oid-none":
-        const options2 = <TnsOaUnsafeProviderOptions>this.client.provider.options;
+      case "oid-none":
+        const options2 = <TnsOaUnsafeProviderOptions>(
+          this.client.provider.options
+        );
         body = querystring.stringify({
           grant_type: "refresh_token",
           refresh_token: this.client.tokenResult.refreshToken,
@@ -146,17 +158,27 @@ export class TnsOAuthClientConnection {
         headers: headers,
         content: body
       })
-      .then((response: http.HttpResponse) => {
-        if (response.statusCode !== 200) {
+      .then(
+        (response: http.HttpResponse) => {
+          if (response.statusCode !== 200) {
+            this.completion(
+              null,
+              response,
+              new Error(
+                `Failed refresh token with status ${response.statusCode}.`
+              )
+            );
+          } else {
+            this.completion(null, response, null);
+          }
+        },
+        error =>
           this.completion(
             null,
-            response,
-            new Error(`Failed refresh token with status ${response.statusCode}.`)
-          );
-        } else {
-          this.completion(null, response, null);
-        }
-      });
+            error,
+            new Error(`Failed refresh token due to network error.`)
+          )
+      );
   }
 
   private getTokenFromCode(

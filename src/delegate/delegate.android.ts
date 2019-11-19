af1f8752dee980e3f0ea964a5d19be67dc6ca24a
Alexander Ziskind <alex@nuvious.com>
2019-11-19 15:27:03 -0500

    ensure _client before calling resumeWithUrl

--- src/delegate/delegate.android.ts
+++ src/delegate/delegate.android.ts
@@ -3,22 +3,24 @@ import * as appModule from "tns-core-modules/application";
 import { TnsOAuthClient } from "../index";
 
 appModule.android.on(
-    appModule.AndroidApplication.activityResumedEvent,
-    (args) => {
-      if (
-          new String(args.activity.getIntent().getAction()).valueOf() ===
-          new String(android.content.Intent.ACTION_VIEW).valueOf()
-      ) {
-          const url = args.activity
-              .getIntent()
-              .getData()
-              .toString();
-          TnsOAuthClientAppDelegate._client.resumeWithUrl(url);
-          console.log(args.activity.getIntent().getData());
-      } else {
-          TnsOAuthClientAppDelegate._client.resumeWithUrl(null);
+  appModule.AndroidApplication.activityResumedEvent,
+  (args) => {
+    if (
+      new String(args.activity.getIntent().getAction()).valueOf() ===
+      new String(android.content.Intent.ACTION_VIEW).valueOf()
+    ) {
+      const url = args.activity
+        .getIntent()
+        .getData()
+        .toString();
+      TnsOAuthClientAppDelegate._client.resumeWithUrl(url);
+      console.log(args.activity.getIntent().getData());
+    } else {
+      if (TnsOAuthClientAppDelegate._client) {
+        TnsOAuthClientAppDelegate._client.resumeWithUrl(null);
       }
     }
+  }
 );
 
 export class TnsOAuthClientAppDelegate {

e22f60acdcda4640dd4dba6636209cb337b04c25
Natasa Vukovic <n.vukovic@levi9.com>
2020-03-27 16:05:04 +0100

    Changed code verifier and code challenger in pkce util

--- src/pkce-util.ios.ts
+++ src/pkce-util.ios.ts
@@ -1,28 +1,18 @@
+import * as CryptoJS from 'crypto-js';
+
 const SHA256_DIGEST_LENGTH = 32;
 
 export function getCodeVerifier(): string {
-  const randomData = NSMutableData.dataWithLength(SHA256_DIGEST_LENGTH);
-  const result: number = SecRandomCopyBytes(kSecRandomDefault, randomData.length, randomData.mutableBytes);
-  if (result !== 0) {
-    return null;
-  } else {
-    return encodeBase64urlNoPadding(randomData);
-  }
+  return encodeBase64urlNoPadding(CryptoJS.lib.WordArray.random(SHA256_DIGEST_LENGTH));
 }
 
 export function sha256base64encoded(inputString: string): string {
-  const verifierData: NSData = NSString.stringWithString(inputString).dataUsingEncoding(NSUTF8StringEncoding);
-  const sha256Verifier: NSMutableData = NSMutableData.dataWithLength(SHA256_DIGEST_LENGTH);
-  CC_SHA256(verifierData.bytes, verifierData.length, <string><unknown>sha256Verifier.mutableBytes);
-  return encodeBase64urlNoPadding(sha256Verifier);
+  return encodeBase64urlNoPadding(CryptoJS.SHA256(inputString));
 }
 
-function encodeBase64urlNoPadding(data: NSData): string {
-  let base64string = data.base64EncodedStringWithOptions(0);
-  // converts base64 to base64url
-  base64string = base64string.replace(/\+/g, "-");
-  base64string = base64string.replace(/\//g, "_");
-  // strips padding
-  base64string = base64string.replace(/=/g, "");
-  return base64string;
+function encodeBase64urlNoPadding(data: any): string {
+  return data.toString(CryptoJS.enc.Base64)
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
 }

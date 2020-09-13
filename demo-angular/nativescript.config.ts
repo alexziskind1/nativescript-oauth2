8be101b66a0ff9a03cc2401d6ea48c4a00f669ba
Alexander Ziskind <alex@nuvious.com>
2020-09-12 21:11:29 -0400

    angular demo update

--- demo-angular/nativescript.config.ts
+++ demo-angular/nativescript.config.ts
@@ -1,10 +1,10 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from "@nativescript/core";
 
 export default {
-  id: 'org.nativescript.ng10',
-  appResourcesPath: 'App_Resources',
-  android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
+    id: "org.nativescript.demoangular",
+    appResourcesPath: "App_Resources",
+    android: {
+        v8Flags: "--expose_gc",
+        markingMode: "none",
+    },
 } as NativeScriptConfig;

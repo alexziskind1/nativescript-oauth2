04dc74904eede1c0ccf8c73d21f4b567df629a0d
triniwiz <fortune.osei@yahoo.com>
2021-10-13 18:11:19 -0400

    fix(android): handle bouncycastle MessageDigest

--- src/pkce-util.android.ts
+++ src/pkce-util.android.ts
@@ -1,6 +1,7 @@
 const DEFAULT_CODE_VERIFIER_ENTROPY = 64;
 const PKCE_BASE64_ENCODE_SETTINGS = android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING | android.util.Base64.URL_SAFE;
 
+declare const org;
 export function getCodeVerifier(): string {
   const randomBytes = Array.create("byte", DEFAULT_CODE_VERIFIER_ENTROPY);
   new java.security.SecureRandom().nextBytes(randomBytes);
@@ -9,7 +10,25 @@ export function getCodeVerifier(): string {
 
 export function sha256base64encoded(codeVerifier: string): string {
   const sha256Digester = java.security.MessageDigest.getInstance("SHA-256");
-  sha256Digester.update(new java.lang.String(codeVerifier).getBytes("ISO_8859_1"));
-  const digestBytes = sha256Digester.digest();
-  return android.util.Base64.encodeToString(digestBytes, PKCE_BASE64_ENCODE_SETTINGS);
+  sha256Digester.update(
+    new java.lang.String(codeVerifier).getBytes("ISO_8859_1")
+  );
+  let digestBytes;
+  if (
+    typeof sha256Digester.digest !== "function" &&
+    sha256Digester.digest instanceof
+      org.bouncycastle.crypto.digests.SHA256Digest
+  ) {
+    const digest = sha256Digester.digest;
+    const size = digest.getDigestSize();
+    digestBytes = Array.create("byte", size);
+    digest.doFinal(digestBytes, 0);
+  } else {
+    digestBytes = sha256Digester.digest();
+  }
+
+  return android.util.Base64.encodeToString(
+    digestBytes,
+    PKCE_BASE64_ENCODE_SETTINGS
+  );
 }
